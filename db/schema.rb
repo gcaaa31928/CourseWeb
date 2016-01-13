85925b08b5d1a7852cc05b136e1577b106826273
gcaaa31928 <gcaaa31928@gmail.com>
2016-01-13 17:34:15 +0800

    Added Course Students

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151025173635) do
+ActiveRecord::Schema.define(version: 20151104041546) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "admins", force: :cascade do |t|
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.string   "access_token"
+  end
 
   create_table "courses", force: :cascade do |t|
     t.string   "name"
@@ -41,6 +47,7 @@ ActiveRecord::Schema.define(version: 20151025173635) do
     t.integer  "score_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "timelog_id"
   end
 
   create_table "scores", force: :cascade do |t|
@@ -52,18 +59,20 @@ ActiveRecord::Schema.define(version: 20151025173635) do
   end
 
   create_table "students", force: :cascade do |t|
-    t.string   "email"
-    t.string   "password"
-    t.boolean  "require_change_password"
-    t.datetime "created_at",              null: false
-    t.datetime "updated_at",              null: false
+    t.string   "name"
+    t.string   "class"
+    t.integer  "course_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.string   "access_token"
   end
 
   create_table "teaching_assistants", force: :cascade do |t|
     t.string   "name"
     t.string   "class"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.string   "access_token"
   end
 
   create_table "teaching_assistants_privileges", id: false, force: :cascade do |t|
