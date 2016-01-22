65f2eeb56d8b55e633f9a0ec521f74f6e24ec11b
Red <redhuang@bearsoft.com.tw>
2016-01-23 02:00:57 +0800

    feat(): Added started page
    added started page, and changed default page to it

--- db/schema.rb
+++ db/schema.rb
@@ -71,15 +71,15 @@ ActiveRecord::Schema.define(version: 20160119135724) do
 
   create_table "teaching_assistants", force: :cascade do |t|
     t.string   "name"
-    t.string   "class"
+    t.integer  "course_id"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
     t.string   "access_token"
   end
 
   create_table "teaching_assistants_privileges", id: false, force: :cascade do |t|
-    t.integer "privilege_id"
     t.integer "teaching_assistant_id"
+    t.integer "privilege_id"
   end
 
   create_table "timelogs", force: :cascade do |t|
