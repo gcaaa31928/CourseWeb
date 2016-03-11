b51c13e5964ecc6c92b76c2df9b2ab620f97f867
test server <test server>
2016-03-11 14:30:51 +0800

    compile bash file

---
+++ compile.sh
@@ -0,0 +1,6 @@
+sudo RAILS_ENV=production bundle exec rake db:migrate
+sudo RAILS_ENV=production bundle exec rake assets:clean
+sudo RAILS_ENV=production bundle exec rake assets:clobber
+sudo RAILS_ENV=production bundle exec rake assets:precompile
+sudo service thin stop
+sudo service thin start
